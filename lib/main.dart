8a7f22ca93b90be3c2673552039540d457e001b9
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-02 19:59:37 +0530

    feat: learn show in import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'dart:developer';
-
+import 'dart:developer' show log;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
